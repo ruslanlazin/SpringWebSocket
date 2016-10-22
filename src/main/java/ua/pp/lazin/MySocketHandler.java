fe84a7943af8d2fad684dd1e448a7de962ae58f7
an <an_datsyuk@i.ua>
2016-10-22 17:11:04 +0300

    first commit

---
+++ src/main/java/ua/pp/lazin/MySocketHandler.java
@@ -0,0 +1,21 @@
+package ua.pp.lazin;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+
+public class MySocketHandler extends TextWebSocketHandler {
+
+
+    @Override
+    public  void handleTextMessage(WebSocketSession session, TextMessage text) throws IOException {
+
+        String message = text.getPayload();
+        message +="fromServer";
+        TextMessage newText = new TextMessage(message);
+        session.sendMessage(newText);
+    }
+}
---
+++ src/main/java/ua/pp/lazin/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package ua.pp.lazin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebMvc
+@EnableWebSocket
+public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+webSocketHandlerRegistry.addHandler(handler(), "/app");
+    }
+    @Bean
+    public WebSocketHandler handler(){
+        return new MySocketHandler();
+    }
+
+}
